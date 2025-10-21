165a018694f6c5a70f773611f5ebabc57aa34e79
KMoran00 <kirshamoranest@gmail.com>
2025-10-21 10:42:30 +0200

    cliente.php acabado

--- DWES/Practica_PDO/cliente.php
+++ DWES/Practica_PDO/cliente.php
@@ -21,6 +21,49 @@ class Cliente{
 
     }
 
+    //Getters
+    public function getDni(){
+        return $this->dni;
+    }
+    public function getNombre(){
+        return $this->nombre;
+    }
+    public function getDireccion(){
+        return $this->direccion;
+    }
+    public function getLocalidad(){
+        return $this->localidad;
+    }
+    public function getProvincia(){
+        return $this->provincia;
+    }
+    public function getTelefono(){
+        return $this->telefono;
+    }
+    public function getEmail(){
+        return $this->email;
+    }
+
+    //Setters
+    public function setNombre($nombre){
+        $this->nombre = $nombre;  
+    }
+    public function setDireccion($direccion){
+        $this->direccion = $direccion;
+    }
+    public function setLocalidad($localidad){
+        $this->localidad = $localidad;
+    }
+    public function setProvincia($provincia){
+        $this->provincia = $provincia;
+    }
+    public function setTelefono($telefono){
+        $this->telefono = $telefono;
+    }
+    public function setEmail($email){
+        $this->email = $email;  
+    }
+
 }
 
 ?>
