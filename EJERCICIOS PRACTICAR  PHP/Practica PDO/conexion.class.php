57066b25d8ded96ce694074c15d29e4ea3aaf57d
KMoran00 <kirshamoranest@gmail.com>
2025-11-20 10:45:18 +0100

    avances conexion class

---
+++ EJERCICIOS PRACTICAR  PHP/Practica PDO/conexion.class.php
@@ -0,0 +1,25 @@
+<?php
+class Conexion
+{
+    //Atributos
+    private $dbname;
+    private $host;
+    private $user;
+    private $password;
+
+    //Constructor de la clase
+    public function __construct($dbname, $host, $user, $password)
+    {
+        $this . $dbname->$dbname;
+        $this . $host->$host;
+        $this . $user->$user;
+        $this . $password->$password;
+    }
+
+    static function conectarBD($dbname, $host, $user, $password){
+        $dbname="Kirsha";
+        $host="10.2.218.1";
+        $user="tendaFake";
+
+    }
+}
