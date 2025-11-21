172ebf617f4f0c363421cd0abe7324560a2fcbfb
KMoran00 <kirshamoranest@gmail.com>
2025-11-21 12:07:55 +0100

    conexion.class.php - Acabado

--- EJERCICIOS PRACTICAR  PHP/Practica PDO/conexion.class.php
+++ EJERCICIOS PRACTICAR  PHP/Practica PDO/conexion.class.php
@@ -7,6 +7,42 @@ class Conexion
     private $user;
     private $password;
 
+    //Getters
+    public function getDbName()
+    {
+        return $this->dbname;
+    }
+    public function getHost()
+    {
+        return $this->host;
+    }
+    public function getUser()
+    {
+        return $this->user;
+    }
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    //Setters
+    public function setDbName($dbname)
+    {
+        $this->dbname = $dbname;
+    }
+    public function setHost($host)
+    {
+        $this->host = $host;
+    }
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+
     //Constructor de la clase
     public function __construct($dbname, $host, $user, $password)
     {
@@ -16,10 +52,24 @@ class Conexion
         $this . $password->$password;
     }
 
-    static function conectarBD($dbname, $host, $user, $password){
-        $dbname="Kirsha";
-        $host="10.2.218.1";
-        $user="tendaFake";
+    static function conectarBD($dbname, $host, $user, $password)
+    {
+        $dbname = "Kirsha";
+        $host = "10.2.218.1";
+        $user = "tendaFake";
+        $password = "Lliurex_01";
 
+        try {
+            $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (Exception $e) {
+            return null;
+            header("Location: index.php");
+            exit();
+        };
+    }
+    static function desconectarBD($pdo)
+    {
+        $pdo = null;
     }
 }
